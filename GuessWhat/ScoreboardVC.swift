5c070d0bb8669450e8efa47510776c3bbb21ab30
alnodiadnan <alnodiadnan@gmail.com>
2020-06-12 07:36:46 +0700

    Add ScoreboardView & CardPageView into the main storyboard

---
+++ GuessWhat/ScoreboardVC.swift
@@ -0,0 +1,30 @@
+//
+//  ScoreboardVC.swift
+//  GuessWhat
+//
+//  Created by Alnodi Adnan on 12/06/20.
+//  Copyright © 2020 Jacob Andrean. All rights reserved.
+//
+
+import UIKit
+
+class ScoreboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
