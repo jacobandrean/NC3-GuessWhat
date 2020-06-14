0b745c22dfed2a267abbd7fd1f416697cc3f423f
amandagreta <62106342+amandagreta@users.noreply.github.com>
2020-06-14 16:50:25 +0700

    How to play dan butterfly

---
+++ GuessWhat/view2.swift
@@ -0,0 +1,23 @@
+//
+//  view2.swift
+//  GuessWhat
+//
+//  Created by Amanda Greta on 13/06/20.
+//  Copyright © 2020 Jacob Andrean. All rights reserved.
+//
+
+import UIKit
+
+class view2: UIView {
+    @IBOutlet weak var labelV: UILabel!
+    @IBOutlet weak var imgV: UIImageView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
